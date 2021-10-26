a6dd0555d184670a71744261d066e2932d6e11ba
Roman Posevin <posevin.2011@mail.ru>
2021-10-26 22:07:40 +0300

    changed project structure

---
+++ labs_data/housing_7_data.py
@@ -0,0 +1,36 @@
+FILENAME = 'labs_data/7_housing.xlsx'
+ALL_COLUMNS = [
+    'CRIM',
+    'ZN',
+    'INDUS',
+    'CHAS',
+    'NOX',
+    'RM',
+    'AGE',
+    'DIS',
+    'RAD',
+    'TAX',
+    'PTRATIO',
+    'B',
+    'LSTAT',
+    'MEDV',
+]
+USEFUL_COLUMNS = []
+TARGET_COLUMNS = [
+    'CRIM',
+]
+FEATURE_COLUMNS = [
+    'ZN',
+    'INDUS',
+    'CHAS',
+    'NOX',
+    'RM',
+    'AGE',
+    'DIS',
+    'RAD',
+    'TAX',
+    'PTRATIO',
+    'B',
+    'LSTAT',
+    'MEDV',
+]
